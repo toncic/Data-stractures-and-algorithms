130b92c33d0683526e378deb45cbfedc55cff7ba
Milan Toncic <milan.toncic@wunder.io>
2020-04-09 23:44:13 +0300

    Updating npm scripts and adding tslint-config-prittier.

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('The sedulous hyena ate the antelope');
+});
+app.listen(port, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express = require("express");
+import express = require('express');
 
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope");
+app.get('/', (req, res) => {
+  res.send('The sedulous hyena ate the antelope');
 });
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     return console.error(err);
   }
